c5976971edb39090e40ebb940577850bec4e2ad2
Elijah Adegede <40616780+elijah0807@users.noreply.github.com>
2023-02-06 05:41:53 +0100

    drawer completed

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -3,6 +3,10 @@ import * as React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { colors } from "../constants";
 import MainNavigator from "./Main";
+import { EvilIcons, Ionicons, Octicons } from "@expo/vector-icons";
+import SettingsScreen from "../screens/settings/Settings";
+import HistoryScreen from "../screens/history/History";
+import LocationScreen from "../screens/location/Location";
 
 const Drawer = createDrawerNavigator();
 
@@ -23,8 +27,76 @@ const DrawerNav = () => {
         component={MainNavigator}
         options={{
           title: "Home",
+          headerShown: false,
+          headerTitle: "MuvaApp",
+          headerTitleStyle: { color: colors.black },
+          headerTintColor: colors.primary,
           drawerActiveBackgroundColor: colors.white,
-          drawerActiveTintColor: colors.black,
+          drawerActiveTintColor: colors.primary,
+          drawerIcon: ({ focused }) => (
+            <Octicons
+              name="home"
+              size={24}
+              color={focused ? colors.primary : colors.black}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          title: "Settings",
+          headerShown: true,
+          headerTitleStyle: { color: colors.black },
+          headerTintColor: colors.primary,
+          drawerActiveBackgroundColor: colors.white,
+          drawerActiveTintColor: colors.primary,
+          drawerIcon: ({ focused }) => (
+            <Ionicons
+              name="settings-outline"
+              size={24}
+              color={focused ? colors.primary : colors.black}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="History"
+        component={HistoryScreen}
+        options={{
+          title: "History",
+          headerShown: true,
+          headerTitleStyle: { color: colors.black },
+          headerTintColor: colors.primary,
+          drawerActiveBackgroundColor: colors.white,
+          drawerActiveTintColor: colors.primary,
+          drawerIcon: ({ focused }) => (
+            <Octicons
+              name="history"
+              size={24}
+              color={focused ? colors.primary : colors.black}
+            />
+          ),
+        }}
+      />
+      <Drawer.Screen
+        name="Location"
+        component={LocationScreen}
+        options={{
+          title: "Location",
+          headerShown: true,
+          headerTitleStyle: { color: colors.black },
+          headerTintColor: colors.primary,
+          drawerActiveBackgroundColor: colors.white,
+          drawerActiveTintColor: colors.primary,
+          drawerIcon: ({ focused }) => (
+            <EvilIcons
+              name="location"
+              size={24}
+              color={focused ? colors.primary : colors.black}
+            />
+          ),
         }}
       />
     </Drawer.Navigator>
---
+++ src/screens/history/History.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Content from "../../components/Content";
+
+const HistoryScreen = () => {
+  return (
+    <Content>
+      <Text>HistoryScreen</Text>
+    </Content>
+  );
+};
+
+export default HistoryScreen;
