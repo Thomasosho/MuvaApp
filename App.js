d94e378b1225c64d3ef7a0588cafe80a48d89214
Thomasosho <tomasosho@gmail.com>
2023-02-07 08:50:43 +0100

    updates

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Navigation from "./src/navigation";
 import "react-native-gesture-handler";
 
 export default function App() {
-  return <IndexNav />;
+  return <IndexNav />
 }
 
 const styles = StyleSheet.create({
